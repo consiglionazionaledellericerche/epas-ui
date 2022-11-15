b932a1fafde161a0e92222df40eb60e259c8f6fc
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2022-11-15 15:01:10 +0100

    Migliorata grafica info mensili

--- components/monthRecap/hoursRecap.tsx
+++ components/monthRecap/hoursRecap.tsx
@@ -14,51 +14,8 @@ const HoursRecap: React.FC<HoursRecapProps> = ({
   }) => {
     return (
         <>
-        {monthRecap.contractMonths.map((contractMonth: ContractMonth, index: number) => (            
-            <Accordion.Item eventKey="0" key={index}>
-            {/* Verificare che sia l'ultimo contratto nel mese */}
-            {/*
-                #{if mese.contract.isLastInMonth((int)psDto.month, (int)psDto.year)}
-                  ... implementazione attuale
-                #{/if}
-                #{else}
-                    <p class="text-error" style="margin-top: 5px;"><em>Il contratto e' terminato nel corso di questo mese.</em></p>
-                #{/else}
-             */}
-            <Accordion.Header>Riepilogo ore</Accordion.Header>
-            <Accordion.Body>
-                <h4>Info generiche</h4>
-                <Table bordered>
-                    <tr>
-                        <td className="col-xs-10">Il numero di giorni lavorativi in sede è di: </td>
-                        <td className="col-xs-2 success">{monthRecap.basedWorkingDays}</td>
-                    </tr>
-                    <tr>
-  	                    {!monthRecap.topQualification &&
-                        <>
-                            <td className="col-xs-10">Tempo disponibile per straordinari: </td>
-                            <td className="col-xs-2 {contractMonth.progressivoFinalePositivoMese=== 0 ? 
-                                'warning' : contractMonth.progressivoFinalePositivoMese > 0 ? : 'success' : 'danger'}">
-                                {DateUtility.fromMinuteToHourMinute(contractMonth.progressivoFinalePositivoMese)} ore
-                            </td>    
-                        </>
-                        }    
-                    </tr>
-                    <tr>
-                        <td className="col-xs-10">Il numero di riposi compensativi usati nell&apos;anno è </td>
-                        <td className="col-xs-2 success">{monthRecap.numberOfCompensatoryRestUntilToday}</td>
-                    </tr>
-                    <tr>
-                        <td className="col-xs-10">
-                            È possibile utilizzare il residuo dell&apos;anno precedente? </td>
-                            {contractMonth.possibileUtilizzareResiduoAnnoPrecedente ?
-    	                        <td className="col-xs-2 success">Sì</td> :
-                                <td className="col-xs-2 danger">No</td>
-                            }
-                    </tr>
-                </Table>              
-            </Accordion.Body>
-        </Accordion.Item>
+        {monthRecap.contractMonths.map((contractMonth: ContractMonth, index: number) => (   
+            <HoursRecapGenericInfo monthRecap={monthRecap} contractMonth={contractMonth} key={index} />
         ))}
         </>
     )
--- components/monthRecap/hoursRecapGenericInfo.tsx
+++ components/monthRecap/hoursRecapGenericInfo.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Accordion, Table } from "react-bootstrap";
-import AccordionItem from "react-bootstrap/esm/AccordionItem";
 import { ContractMonth } from "../../types/contractMonth";
 import { MonthRecap } from "../../types/monthRecap";
 import DateUtility from "../../utils/dateUtility";
@@ -8,16 +7,68 @@ import DateUtility from "../../utils/dateUtility";
 interface HoursRecapGenericInfoProps {
     monthRecap: MonthRecap, 
     contractMonth: ContractMonth
-    idx: number
 }
 
 const HoursRecapGenericInfo: React.FC<HoursRecapGenericInfoProps> = ({
-    monthRecap, contractMonth, idx
+    monthRecap, contractMonth
   }) => {
+    function progressivoStyleClass(progressivo : number) {
+        let progressivoStyle : string = "col-xs-2 success"
+        if (progressivo === 0) {
+            progressivoStyle = "col-xs-2 warning"
+        }
+        if (progressivo < 0) {
+            progressivoStyle = "col-xs-2 danger"
+        }
+        return progressivoStyle
+    }
     return (
-        <>
-        empty
-        </>
+
+        <Accordion.Item eventKey="0">
+        {/* Verificare che sia l'ultimo contratto nel mese */}
+        {/*
+            #{if mese.contract.isLastInMonth((int)psDto.month, (int)psDto.year)}
+                ... implementazione attuale
+            #{/if}
+            #{else}
+                <p class="text-error" style="margin-top: 5px;"><em>Il contratto e' terminato nel corso di questo mese.</em></p>
+            #{/else}
+        */}
+        <Accordion.Header>Riepilogo ore</Accordion.Header>
+        <Accordion.Body>
+            <h5>Info generiche</h5>
+            <Table bordered className="riepilogo-ore table-sm">
+            <tbody>
+                <tr>
+                    <td className="col-xs-10">Il numero di giorni lavorativi in sede è di: </td>
+                    <td className="col-xs-2 success">{monthRecap.basedWorkingDays}</td>
+                </tr>
+                <tr>
+  	                {!monthRecap.topQualification &&
+                    <>
+                        <td className="col-xs-10">Tempo disponibile per straordinari: </td>
+                        <td className={progressivoStyleClass(contractMonth.progressivoFinaleMese)}>
+                            {DateUtility.fromMinuteToHourMinute(contractMonth.progressivoFinalePositivoMese)} ore
+                        </td>
+                    </>
+                    }
+                </tr>
+                <tr>
+                    <td className="col-xs-10">Il numero di riposi compensativi usati nell&apos;anno è </td>
+                    <td className="col-xs-2 success">{monthRecap.numberOfCompensatoryRestUntilToday}</td>
+                </tr>
+                <tr>
+                    <td className="col-xs-10">
+                        È possibile utilizzare il residuo dell&apos;anno precedente? </td>
+                        {contractMonth.possibileUtilizzareResiduoAnnoPrecedente ?
+    	                    <td className="col-xs-2 success">Sì</td> :
+                                <td className="col-xs-2 danger">No</td>
+                        }
+                </tr>
+            </tbody>
+            </Table>              
+        </Accordion.Body>
+    </Accordion.Item>
     )
 }
 
