8e1fde3fc662924a2f157ce357db12422ee25c81
Andrea Generosi <and_gen@hotmail.com>
2023-04-04 20:02:46 +0200

    fix problemi con signout che non chiamava logout di keycloak

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
     return [
-//      {
-//        source: '/api(^(?!.*\/auth\/).*$)/:path*',
-////        source: '/api/rest/:path*',
-//        destination: 'http://localhost:8080/:path*' // Proxy to Backend
-//      }
       {
               source: '/api/auth/:path*',
               destination: '/api/auth/:path*',
