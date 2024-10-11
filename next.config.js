d04b404bc138168b72c0365c940a3b7f44532144
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2024-10-11 12:53:00 +0200

    Ripulita configurazione variabili env

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const epasService = 'NEXT_PUBLIC_EPAS_SERVICE';
-const apiDestination = process.env[epasService] || process.env.EPAS_SERVICE;
-
 const nextConfig = {
   env: {
-      CLIENTID: process.env.NEXT_PUBLIC_CLIENTID || process.env.CLIENTID || "",
-      CLIENTSECRET: process.env.NEXT_PUBLIC_CLIENT_SECRET || process.env.CLIENT_SECRET || "",
-      NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL || process.env.NEXTAUTH_URL || "",
-      NETXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET || process.env.NEXTAUTH_SECRET || "",
+      CLIENTID: process.env.NEXT_PUBLIC_CLIENTID,
+      CLIENTSECRET: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+      NETXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
       OAUTH_LOGIN: process.env.NEXT_PUBLIC_OAUTH_LOGIN || "true",
       LDAP_LOGIN: process.env.NEXT_PUBLIC_LDAP_LOGIN || "false",
-      EPAS_SERVICE: process.env[epasService] || process.env.EPAS_SERVICE || "",
-      EPAS_HELPDESK_SERVICE: process.env.NEXT_PUBLIC_EPAS_HELPDESK_SERVICE || process.env.EPAS_HELPDESK_SERVICE || "",
+      EPAS_SERVICE: process.env.NEXT_PUBLIC_EPAS_SERVICE,
+      EPAS_HELPDESK_SERVICE: process.env.NEXT_PUBLIC_EPAS_HELPDESK_SERVICE,
     },
   reactStrictMode: true,
   swcMinify: true,
@@ -24,7 +21,7 @@ const nextConfig = {
       },
       {
         source: '/api/:path*',
-        destination: apiDestination // Proxy to Backend
+        destination: process.env.NEXT_PUBLIC_EPAS_SERVICE // Proxy to Backend
       },
     ]
   },
