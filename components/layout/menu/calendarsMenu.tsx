1ca4c29f3ac5a8f43105c83a940b93825abc393b
Andrea Generosi <and_gen@hotmail.com>
2025-01-10 19:50:23 +0100

    sistemato menu a seconda del tipo di utente che si connette al portale

--- components/layout/menu/calendarsMenu.tsx
+++ components/layout/menu/calendarsMenu.tsx
@@ -5,7 +5,7 @@ import { NavDropdown } from "react-bootstrap"
 function CalendarsMenu() {
     return (        
         <NavDropdown title="Calendari" id="calendars">
-          <NavDropdown.Item href="/stampings"><FontAwesomeIcon icon={faCalendarDays} /> Calendario turni</NavDropdown.Item>
+          <NavDropdown.Item href="/reperibility"><FontAwesomeIcon icon={faCalendarDays} /> Calendario turni</NavDropdown.Item>
           <NavDropdown.Item href="/reperibility"><FontAwesomeIcon icon={faCalendarDays} /> Calendario reperibilit&agrave;</NavDropdown.Item>
         </NavDropdown>
     )
